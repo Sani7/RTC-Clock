699171483c049ff2b820d2cd32c91e51cbe9f42a
Sander Speetjens <sander.speetjens@gmail.com>
2021-07-01 17:32:51 +0200

    Added the __main__

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from clock import *
 
-try:
-        while (1):
-                (Digit1, Digit2, Digit3, Digit4, Leds) = GetTimeToDigit()
-                DisplayDigit(Digit1, 1, 1)
-                Delay()
-                DisplayDigit(Digit2, 1, 2)
-                Delay()
-                DisplayDigit(Digit3, 1, 3)
-                Delay()
-                DisplayDigit(Digit4, 1, 4)
-                Delay()
-                DisplayLeds(Leds)
+if __name__ == "__main__":
+        try:
+                while (1):
+                        (Digit1, Digit2, Digit3, Digit4, Leds) = GetTimeToDigit()
+                        DisplayDigit(Digit1, 1, 1)
+                        Delay()
+                        DisplayDigit(Digit2, 1, 2)
+                        Delay()
+                        DisplayDigit(Digit3, 1, 3)
+                        Delay()
+                        DisplayDigit(Digit4, 1, 4)
+                        Delay()
+                        DisplayLeds(Leds)
 
-except KeyboardInterrupt:
-        GPIO.cleanup()
+        except KeyboardInterrupt:
+                GPIO.cleanup()
